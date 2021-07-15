e56a4e0231c22d305704310937c592d87e6deef5
eseo128 <34049770+eseo128@users.noreply.github.com>
2021-07-16 03:02:38 +0900

    search

--- src/app.jsx
+++ src/app.jsx
@@ -1,22 +1,47 @@
 import React, { useEffect, useState } from 'react';
-import './app.css';
-import VideoList from './componets/video_list/video_list';
+import styles from './app.module.css';
+import SearchHeader from './components/search_header/search_header';
+import VideoList from './components/video_list/video_list';
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const search = query => {
+    //포스트맨 search API 불러오기
 
-  useEffect(() => {
-    const requestOptions = {
-     method: 'GET',
-     redirect: 'follow'
+  const requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
 };
 
-fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResult=25&key=AIzaSyDFmnSko-nXIO9qNH40be8XTPOXEIXuE2A", requestOptions)
+fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&key=AIzaSyDFmnSko-nXIO9qNH40be8XTPOXEIXuE2A`, requestOptions)
   .then(response => response.json())
   .then(result => setVideos(result.items))
   .catch(error => console.log('error', error));
+
+  }
+
+  useEffect(() => {
+    //포스트맨 Most Popular API 불러오기
+
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDFmnSko-nXIO9qNH40be8XTPOXEIXuE2A",
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
   }, []);
-  return <VideoList videos={videos} />;
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search}/>
+      <VideoList videos={videos} />
+  </div>
+  )
 }
 
 export default App;
---
+++ src/components/search_header/search_header.jsx
@@ -0,0 +1,40 @@
+import styles from './search_header.module.css';
+import React, { useRef } from 'react';
+
+
+const SearchHeader = ({onSearch}) => {
+    const inputRef = useRef(); //메모
+    const handleSearch = () => {
+        const value = inputRef.current.value;
+        //console.log(value); //검색단어 받아오는지 확인
+        
+        onSearch(value);
+        //검색하는 것을 props로 받아오기
+    }
+    const onClick = () => {
+        //console.log('onClick') 확인해보기
+        handleSearch();
+    }
+
+    const onKeyPress = (event) => {
+        //console.log('onKeyPress') 확인해보기
+        if (event.key === 'Enter') {
+            handleSearch();
+        }
+    }
+
+    return (
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <img className={styles.img} src="youtube/images/logo.png" alt="logo" />
+            <h1 className={styles.title}>Youtube</h1>
+            </div>
+            <input ref={inputRef} className={styles.input} type="search" placeholder="Search..." onKeyPress={onKeyPress}/>
+            <button className={styles.button} type="submit" onClick={onClick}>
+                <img className={styles.buttonImg} src="youtube/images/search.png" alt="search" />
+            </button>
+        </header>
+    )
+};
+
+export default SearchHeader;
