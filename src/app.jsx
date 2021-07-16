5b22aa44745b971c4f843ffc19ab10dfcecf9645
eseo128 <34049770+eseo128@users.noreply.github.com>
2021-07-16 16:21:56 +0900

    add youtube service

--- src/app.jsx
+++ src/app.jsx
@@ -3,45 +3,27 @@ import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
-function App() {
+function App({youtube}) {
   const [videos, setVideos] = useState([]);
   const search = query => {
     //포스트맨 search API 불러오기
-
-  const requestOptions = {
-  method: 'GET',
-  redirect: 'follow'
-};
-
-fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&key=AIzaSyDFmnSko-nXIO9qNH40be8XTPOXEIXuE2A`, requestOptions)
-  .then(response => response.json())
-  .then(result => setVideos(result.items))
-  .catch(error => console.log('error', error));
-
-  }
+    youtube
+      .search(query)//
+      .then(videos => setVideos(videos));
+  };
 
   useEffect(() => {
     //포스트맨 Most Popular API 불러오기
-
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-
-    fetch(
-      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDFmnSko-nXIO9qNH40be8XTPOXEIXuE2A",
-      requestOptions
-    )
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    youtube
+      .mostPopular()//
+      .then(videos => setVideos(videos));
   }, []);
   return (
     <div className={styles.app}>
-      <SearchHeader onSearch={search}/>
+      <SearchHeader onSearch={search} />
       <VideoList videos={videos} />
-  </div>
-  )
+    </div>
+  );
 }
 
 export default App;
